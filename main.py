fdeb515bfb7bff27766049632360e73460e5eb37
Zine Eddine Latioui <Zine.Latioui@gmail.com>
2019-11-26 06:33:28 +0100

    can show video with pygame and opencv

--- main.py
+++ main.py
@@ -1,4 +1,37 @@
 import cv2 
+import pygame
+import numpy as np
+from pygame import display
+
+
+
+# pygame parametres 
+
+display_width = 640
+display_height = 360
+
+gameDisplay = display.set_mode((display_width,display_height))
+
+
+def frameProcessing(image):
+
+    #print(image.shape)
+    heigh, wight, depth = image.shape
+    image =cv2.resize(image,(wight//3,heigh//3))
+    #cv2.imshow('Frame',image)
+    #cv2.imshow('Frame',image)   
+
+
+
+    # if you want to use pygame for showing the vide!
+    surf = pygame.surfarray.make_surface(np.swapaxes(image,0,1))
+    gameDisplay.blit(surf,(0,0))
+    display.update()
+    
+    print(image.shape)
+
+
+
 
 
 
@@ -11,9 +44,11 @@ if (cap.isOpened()==False):
 while(cap.isOpened()):
     ret, frame = cap.read()
     if ret == True:
+        
+        frameProcessing(frame)
 
-        #cv2.imshow('Frame',frame)
-        print('h')
+        
+        
 
         if (cv2.waitKey(25) & 0xFF == ord("q")):
             break
@@ -25,4 +60,3 @@ cap.release()
 cv2.destroyAllWindows()
 
 
-print("hello word")
